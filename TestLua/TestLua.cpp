671c6cf6a902d3d868baa4ecad62aac6ccfb81a0
Christoph <christoph@stoepel.net>
2015-07-04 10:47:42 +0200

    Upgrade to Lua 5.3.1.

--- TestLua/TestLua.cpp
+++ TestLua/TestLua.cpp
@@ -13,7 +13,7 @@ extern "C"
 int _tmain(int argc, _TCHAR* argv[])
 {
 	_tprintf(_T("Initializing lua vm...\n"));
-	lua_State *lua = lua_open();
+	lua_State *lua = luaL_newstate();
 
 	if (lua)
 	{
@@ -21,27 +21,36 @@ int _tmain(int argc, _TCHAR* argv[])
 		luaL_openlibs(lua);
 
 		_tprintf(_T("Loading script...\n"));
-		if (luaL_loadfile(lua, "D:\\Projekte\\Tests\\TestLua\\test.lua") == 0)
+		if (luaL_loadfile(lua, "test.lua") == 0)
 		{
 			lua_pcall(lua, 0, LUA_MULTRET, 0);	// run main
-	
+
+			//_tprintf(_T("Stack height: %d\n"), lua_gettop(lua));
 			_tprintf(_T("Calling script function...\n"));
-			lua_pushstring(lua, "ftest2");	
-			lua_gettable(lua, LUA_GLOBALSINDEX);
-			lua_pushnumber(lua, 1.0);
+			lua_pushglobaltable(lua);
+			lua_pushstring(lua, "ftest2"); // function name
+			lua_gettable(lua, -2);         // ... converted to function
+			lua_pushnumber(lua, 1.0);      // argument
 			if (lua_pcall(lua, 1, 1, 0) == 0)
 			{
 				if (lua_isnumber(lua, -1))
 				{
 					_tprintf(_T("Result: %f\n"), lua_tonumber(lua, -1));
 				}
-				lua_pop(lua, 1);
+				lua_pop(lua, 2); // clear return value and global table
 			}
 			else
 			{
+#ifdef UNICODE
+				WCHAR szMsg[255];
+				::MultiByteToWideChar(CP_ACP, MB_PRECOMPOSED, lua_tostring(lua, -1), -1, szMsg, 255);
+				_tprintf(_T("Error in Script: %s\n"), szMsg);
+#else
 				_tprintf(_T("Error in Script: %s\n"), lua_tostring(lua, -1));
-				lua_pop(lua, 1);
+#endif
+				lua_pop(lua, 2); // clear error and global table
 			}
+			//_tprintf(_T("Stack height: %d\n"), lua_gettop(lua));
 		}
 
 		lua_close(lua);
